abc56e616e6abaf58b03bf9e9b21fdf999da0bd4
chescamargarette <franchesca.margarette.visitacion@eee.upd.edu.ph>
2026-05-05 17:59:01 +0800

    Update pins.h

--- header_files/pins.h
+++ header_files/pins.h
@@ -1,1 +1,20 @@
+#ifndef PINS_H
+#define PINS_H
 
+// Motor Pin Definitions
+#define EN_LEFT_MOTOR   12      // Enable left motor
+#define REV_LEFT_MOTOR  13      // reverse left motor
+#define FOR_LEFT_MOTOR  8       // forward left motor
+#define EN_RIGHT_MOTOR  10      // Enable right motor
+#define REV_RIGHT_MOTOR 11      // reverse right motor
+#define FOR_RIGHT_MOTOR 9       // forward right motor
+
+// Line Sensor Pin Definitions
+#define LEFT_SENSOR     6       // Left sensor
+#define MID_SENSOR      3       // Middle sensor
+#define RIGHT_SENSOR    2       // Right sensor
+
+// Onboard LED Pin Definition
+#define ONBOARD_LED     15      // PA15
+
+#endif // PINS_H
