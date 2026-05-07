097bfb425ec7843ee9019d324416b3666c74906b
chescamargarette <franchesca.margarette.visitacion@eee.upd.edu.ph>
2026-05-07 22:39:58 +0800

    Update pins.h

--- header_files/pins.h
+++ header_files/pins.h
@@ -2,12 +2,12 @@
 #define PINS_H
 
 // Motor Pin Definitions
-#define EN_LEFT_MOTOR   12      // Enable left motor
-#define REV_LEFT_MOTOR  13      // reverse left motor
-#define FOR_LEFT_MOTOR  8       // forward left motor
-#define EN_RIGHT_MOTOR  10      // Enable right motor
-#define REV_RIGHT_MOTOR 11      // reverse right motor
-#define FOR_RIGHT_MOTOR 9       // forward right motor
+#define EN_LEFT_MOTOR   8      // Enable left motor
+#define REV_LEFT_MOTOR  10      // reverse left motor
+#define FOR_LEFT_MOTOR  11       // forward left motor
+#define EN_RIGHT_MOTOR  9      // Enable right motor
+#define REV_RIGHT_MOTOR 16      // reverse right motor
+#define FOR_RIGHT_MOTOR 17       // forward right motor
 
 // Line Sensor Pin Definitions 
 #define LEFT_LINE_1     3       // PA03 - Left sensor 1
@@ -19,16 +19,16 @@
 
 // Wall Sensor Pin Definitions
 // Left Wall Sensor
-#define LEFT_WALL_TRIG  20      // PA20 - Left wall trigger
-#define LEFT_WALL_ECHO  14      // PA14 - Left wall echo
+//#define LEFT_WALL_TRIG  20      // PA20 - Left wall trigger
+//#define LEFT_WALL_ECHO  14      // PA14 - Left wall echo
 
 // Middle Wall Sensor
 #define MIDDLE_WALL_TRIG 16     // PA16 - Middle wall trigger
 #define MIDDLE_WALL_ECHO 17     // PA17 - Middle wall echo
 
 // Right Wall Sensor
-#define RIGHT_WALL_TRIG 21      // PA21 - Right wall trigger
-#define RIGHT_WALL_ECHO 22      // PA22 - Right wall echo
+//#define RIGHT_WALL_TRIG 21      // PA21 - Right wall trigger
+//#define RIGHT_WALL_ECHO 22      // PA22 - Right wall echo
 
 // Onboard LED Pin Definition
 #define ONBOARD_LED     15      // PA15
