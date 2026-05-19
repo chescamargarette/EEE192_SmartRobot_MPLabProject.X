5ce3c333a37557e5544c33f115b78324ac9f135a
chescamargarette <franchesca.margarette.visitacion@eee.upd.edu.ph>
2026-05-19 17:30:31 +0800

    Update motor.c

--- source_files/motor.c
+++ source_files/motor.c
@@ -7,8 +7,8 @@
 #include "motor.h"
 
 // Initialize Global Variables
-extern int alternate_state = 0;
-extern unsigned long last_alternate_time = 0;
+extern int alternate_state = 0;                 // to be updated in main.c
+extern unsigned long last_alternate_time = 0;   // to be updated in main.c
 
 // Function Definitions
 void init_motors(void) {
